f517e065ea2900d8c4de6ce3ab5c8277a63caab8
wunnavabk <wunnavabk@gmail.com>
2024-07-06 20:53:11 +0530

    check

---
+++ src/frontend/Tables.js
@@ -0,0 +1,17 @@
+function Tables (head, rows, totalCount) {
+    return (
+        <Box>
+        <DynamicTable
+          head={head}
+          rows={rows ? rows : "Loading...."}
+          rowsPerPage={10}
+        />
+        <Text>
+          Total issues in this project:{" "}
+          <Badge appearance="primary" max={false}>
+            {totalCount}
+          </Badge>
+        </Text>
+      </Box>
+    );
+  }
